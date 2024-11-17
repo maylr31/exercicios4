98e3d13e0653050e0f4f407f0d0d8e0c21c64692
maylr31 <maylopes2122@gmail.com>
2024-11-17 20:21:07 -0300

    exercicio4

--- exercicio4.py
+++ exercicio4.py
@@ -2,4 +2,4 @@ n1= int(input('Digite um numero:'))
 n2= int(input('Digite outro numero:'))
 s= n1 + n2
 print(f'A soma dos numeros é:{s}')
-#prestar atenção nos espaços
+#pode ser a variavel int ou float
